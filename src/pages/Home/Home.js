fd94a1ace5ceae81a3e6b65727ff4939cb3eb00b
Marcelo <cecel.alex@gmail.com>
2019-05-22 12:51:48 -0300

    Trabalhando com requiscoes POST com dados

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,11 +3,60 @@ import { Link } from 'react-router-dom';
 
 export class Home extends Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			lista:[]
+		};
+	}
+	// https://jsonplaceholder.typicode.com/posts
+	componentDidMount() {
+		/*
+		fetch('https://jsonplaceholder.typicode.com/posts')
+			.then(r => r.json())
+			.then(json => {
+				this.setState({lista:json});
+			});
+		*/
+
+		let corpo = {
+			title:'Meu post novo',
+			body:'Conteúdo do post para testes',
+			userId:'1'
+		};
+
+		fetch('https://jsonplaceholder.typicode.com/posts', {
+			method:'POST',
+			body:JSON.stringify(corpo),
+			headers:{
+				"Content-type":"application/json;charset=UTF-8"
+			}
+		})
+		.then(r => r.json())
+		.then(json => {
+			console.log(json);
+		})
+	}
+
 	render() {
 		return (
 			<div>
-				Página Home...
-
+				<h3>Página Home...</h3>
+				<div>
+					{this.state.lista.length == 0 &&
+						<i>Carregando...</i>
+					}
+						{this.state.lista.length > 0 &&
+						<ul>
+							{this.state.lista.map((item)=>{
+								return (
+									<li># {item.id} - {item.title}</li>
+								);
+							})}
+						</ul>
+					}
+				</div>
+				<br/>
 				<Link to="/sobre">Ir para página sobre</Link>
 			</div>
 		);
